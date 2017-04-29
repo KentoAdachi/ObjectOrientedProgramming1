fd190abe9210ddedb9146722c535f6b201880b81
足立賢人 <kent@yvpn026196.sras.sic.shibaura-it.ac.jp>
2017-04-30 02:07:51 +0900

    normal commit

---
+++ practice/GCD/GCD.java
@@ -0,0 +1,16 @@
+/**
+2つの整数の最大公約数を求めます
+@author 足立賢人
+@version 1.0
+*/
+public class GCD{
+  /**
+  2つの整数の最大公約数を計算します
+  @return int
+  @param m 割られる数
+  @param n 割る数
+  */
+  public int calc(int m,int n){
+
+  }
+}
