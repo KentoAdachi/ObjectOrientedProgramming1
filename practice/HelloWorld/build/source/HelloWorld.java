c0e909a215be7d9220e8cd95df7ce78673494402
足立賢人 <kent@owlan051123.sras.sic.shibaura-it.ac.jp>
2017-05-01 13:42:44 +0900

    pudated

---
+++ practice/HelloWorld/build/source/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ハローワールドを出力するクラス
+@author 足立賢人
+@version 1.0
+*/
+public class HelloWorld {
+  /**
+  メインメソッドは,クラスのエントリーポイントです
+  @param args[] コマンドライン引数は,コマンドラインからjavaアプリケーションにパラメータを渡すことができます 
+  */
+  public static void main(String[] args) {
+
+    System.out.println("HelloWorld!");
+  }
+}
