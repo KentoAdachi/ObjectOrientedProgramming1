734487bdf335d60b4bc4b0377b075bc0ddc04800
足立賢人 <kent@MyBookAir.local>
2017-07-09 02:21:57 +0900

    updated

--- 5/kadai5-2/source/com/stack2/Main.java
+++ 5/kadai5-2/source/com/stack2/Main.java
@@ -1,6 +1,7 @@
 package com.stack2;
 class Main{
     public static void main(String[] args) {
+        System.out.println("正しい実行例 : ");
         Stack stack = new Stack();
         System.out.println("stack : " + stack.getHead());
         stack.push("one");
@@ -15,5 +16,13 @@ class Main{
         System.out.println("stack : " + stack.getHead());
         System.out.println("pop : "+stack.pop());
         System.out.println("stack : " + stack.getHead());
+
+        System.out.println("\n正しくない実行例");
+        System.out.println("エラー内容 :");
+        System.out.println("    Exception in thread \"main\" java.lang.NullPointerException");
+        System.out.println("    at com.stack2.Stack.pop(Stack.java:15)");
+        System.out.println("    at com.stack2.Main.main(Main.java:6)");
+        System.out.println("状況 :");
+        System.out.println("    スタックが空の時にpopを実行した時");
     }
 }
